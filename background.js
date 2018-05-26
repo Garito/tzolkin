3359ad34150ebe9c38fc2a19433f31ec4e42b8e9
Garito <garito@gmail.com>
2018-05-27 00:25:21 +0200

    The icon now reloads every load page

--- background.js
+++ background.js
@@ -1,4 +1,14 @@
-let data = todays();
+function changeIcon () {
+  let data = todays();
 
-chrome.browserAction.setIcon({ path: data.kinImg });
-chrome.browserAction.setTitle({ title: data.name });
+  chrome.browserAction.setIcon({ path: data.kinImg });
+  chrome.browserAction.setTitle({ title: data.name });
+}
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+  changeIcon();   
+  }
+})
+
+changeIcon();
